7c3a3930288b16d4e44e6ce36b38ceff65e5c7f4
Rahul Bhiwande <77515814+RahulBhiwandeNU@users.noreply.github.com>
2021-08-19 13:16:40 -0400

    Time: 5 ms (72.26%), Space: 52.7 MB (24.20%) - LeetHub

--- find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
+++ find-k-pairs-with-smallest-sums/find-k-pairs-with-smallest-sums.java
@@ -1,25 +1,34 @@
 class Solution {
     public List<List<Integer>> kSmallestPairs(int[] nums1, int[] nums2, int k) {
-        
-        List<List<Integer>> pairs = new ArrayList<> ();
-        if (nums1.length == 0 || nums2.length == 0 || k == 0) {
-            return pairs;
-        }
-        
-        PriorityQueue<int[]> minHeap = new PriorityQueue<> ((arr1, arr2) -> arr1[0] + arr1[1] - arr2[0] - arr2[1]);
-        
-        for (int i = 0; i < nums1.length && i < k; i++) {
-            minHeap.offer (new int[] {nums1[i], nums2[0], 0});
-        }
-        
-        while (k-- != 0 && !minHeap.isEmpty ()) {
-            int[] curr = minHeap.poll ();
-            pairs.add (List.of (curr[0], curr[1]));
-            if (curr[2] + 1 < nums2.length) {
-                minHeap.offer (new int[] {curr[0], nums2[curr[2] + 1], curr[2] + 1});
-            }
-        }
-        
-        return pairs;
+        int n1 = nums1.length, n2 = nums2.length;
+
+		List<List<Integer>> kSmallestPairs = new ArrayList<>();
+
+        // No pairing possible when n1/n2/k == 0
+		if (n1 == 0 || n2 == 0 || k == 0) {
+			return kSmallestPairs;
+		}
+
+        // Sort by sum of nums1[i], nums2[j]
+		PriorityQueue<int[]> smallestSumPairs = new PriorityQueue<>((a, b) -> a[2] - b[2]);
+
+        // Add min(n1, k) pairs to the heap where each element in nums1 array pairs up with 0th index in nums2 array initially!
+        // Note that index 2 of each pair is the sum of nums1[i], nums2[0] which is used to heapify & thereby pick k smallest pairs
+		for (int i = 0; i < Math.min(n1, k); i++) {
+			smallestSumPairs.add(new int[] { nums1[i], 0, nums1[i] + nums2[0] });
+		}
+
+		while (!smallestSumPairs.isEmpty() && k-- > 0) {
+			int[] currentPair = smallestSumPairs.poll();
+
+			kSmallestPairs.add(Arrays.asList(currentPair[0], nums2[currentPair[1]]));
+
+            // If currentPair[1] (aka 'j' referring to nums2 array) is within bounds? - Add 1 to the current index 'j' to get the next number in nums2 array
+			if (currentPair[1] < n2 - 1) {
+				smallestSumPairs.add(new int[] { currentPair[0], currentPair[1] + 1, currentPair[0] + nums2[currentPair[1] + 1] });
+			}
+		}
+
+		return kSmallestPairs;
     }
 }
