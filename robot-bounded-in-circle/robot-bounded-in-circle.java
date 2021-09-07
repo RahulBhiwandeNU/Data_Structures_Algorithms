e1d465951bd1511b6ed12f49c422c9b91f00d07b
Rahul Bhiwande <77515814+RahulBhiwandeNU@users.noreply.github.com>
2021-09-07 13:24:29 -0400

    Time: 0 ms (100.00%), Space: 37.1 MB (53.21%) - LeetHub

--- robot-bounded-in-circle/robot-bounded-in-circle.java
+++ robot-bounded-in-circle/robot-bounded-in-circle.java
@@ -2,20 +2,18 @@ class Solution {
     public boolean isRobotBounded(String instructions) {
         int[][] directions = new int[][]{{0,1},{1,0},{0,-1},{-1,0}};
         
-        int x = 0 ; 
-        int y = 0 ;
-        
         int facing = 0;
-        int n = directions.length;
+        int x = 0;
+        int y = 0;
         
         for(char c : instructions.toCharArray()){
             if(c == 'L')
-                facing = (facing + 3) % n;
+                facing = (facing + 3) % directions.length;
             else if(c == 'R')
-                facing = (facing + 1) % n;
-            else{
+                facing = (facing + 1) % directions.length;
+            else {
                 x = x + directions[facing][0];
-                y = y + directions[facing][1]; 
+                y = y + directions[facing][1];
             }
         }
         
